a64912103897b9cb362e4c0a9a89d3b01511442f
brandon3055 <brandon4261@gmail.com>
2023-08-05 19:56:24 +1000

    1.18

--- src/main/java/com/brandon3055/csg/CSG.java
+++ src/main/java/com/brandon3055/csg/CSG.java
@@ -2,13 +2,31 @@ package com.brandon3055.csg;
 
 import com.brandon3055.csg.commands.CSGCommand;
 import com.brandon3055.csg.commands.KitsCommand;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import net.minecraft.core.IdMapper;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.GameData;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @Mod(CSG.MODID)
